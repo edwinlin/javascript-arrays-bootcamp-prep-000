afaf880a9d7bcbe5559c71fe0b3bb10b0bd67f58
Edwin Lin <edwinlin@gmail.com>
2018-11-03 04:59:31 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -5,7 +5,7 @@ function addElementToBeginningOfArray(array, element){
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element){
-  var newArray = array.slice(1).unshift(element)
+  var newArray = array.slice(1)
   return newArray
 }
 
