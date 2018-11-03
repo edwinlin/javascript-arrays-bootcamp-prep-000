2e8e08d22ee37f4398a871d8760aceb2a9b57c70
Edwin Lin <edwinlin@gmail.com>
2018-11-03 05:11:48 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -13,8 +13,7 @@ function addElementToEndOfArray(array, element){
 }
 
 function destructivelyAddElementToEndOfArray(array, element){
-  var newArray = array.push(element)
-  return newArray
+  return array.push(element)
 }
 
 function accessElementInArray(array, index){
