14b71503fb9cf2d43642fd731f8d77dd7d632c7a
Edwin Lin <edwinlin@gmail.com>
2018-11-03 02:52:31 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,17 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
+function addElementsToBeginningOfArray(array, element){
+  return [element,...array]
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element){
+  return array.shift().unshift(element)
+}
+
+function addElementToEndOfArray(array, element){
+  
+}
+
+function destructivelyAddElementToEndOfArray(array, element){
+  
+}
