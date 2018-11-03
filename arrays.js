dfcdd9fef432779ded259d4e494400a2d03e8a3c
Edwin Lin <edwinlin@gmail.com>
2018-11-03 05:17:02 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -33,8 +33,8 @@ function removeElementFromBeginningOfArray(array){
 }
 
 function destructivelyRemoveElementFromEndOfArray(array){
-  var newArray = array.slice(0, array.length-1)
-  return newArray
+  array = array.slice(0, array.length-1)
+  return array
 }
 
 function removeElementFromEndOfArray(array){
