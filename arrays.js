001119219478171c33d2ba4d666a78014e6009c4
Edwin Lin <edwinlin@gmail.com>
2018-11-03 05:15:49 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -10,7 +10,7 @@ function destructivelyAddElementToBeginningOfArray(array, element){
 }
 
 function addElementToEndOfArray(array, element){
-  var newArray = [array,...element]
+  var newArray = [...array, element]
   return newArray
 }
 
